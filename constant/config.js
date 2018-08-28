1ed06d50b334ab8315fc74f8eca6ad1748177b7e
hnisi\dupengfei <463552890@qq.com>
2018-08-28 14:20:26 +0800

    配置文件增加集成测试环境

--- constant/config.js
+++ constant/config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  urlPrefix: 'http://localhost:8081',
+  urlPrefix: 'http://sb.sinobest.cn/mini-program-dg',//集成测试环境
+  // urlPrefix: 'http://sb.sinobest.cn/dev_34',//开发环境
+  // urlPrefix: 'http://localhost:80',//本地测试
   validateFace: {
     targetAppid: 'wx75855cd02ed4cfb9',
     appid: '4818',
